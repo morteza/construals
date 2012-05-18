62944c1d36beea7e0be55155c71a402ea13f5c01
Morteza Ansarinia <ansarinia@me.com>
2012-05-18 18:01:04 +0430

    Added LCM, CSVs, and various improvements
    Mainly added linguistic category model and CSV files
    
    Signed-off-by: Morteza Ansarinia <ansarinia@me.com>

---
+++ app/controllers/LCM.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import fuschia.tagger.Bootstrap;
+import fuschia.tagger.common.Document;
+import models.ConstrualData;
+import play.mvc.*;
+
+public class LCM extends Controller {
+
+    public static void index() {
+        render();
+    }
+}
