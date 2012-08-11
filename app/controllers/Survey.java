1546a19f3a455470f37c9003bad456f7b05dfbf3
Morteza Ansarinia <ansarinia@me.com>
2012-08-12 04:22:24 +0430

    update copyright notice

--- app/controllers/Survey.java
+++ app/controllers/Survey.java
@@ -1,11 +1,18 @@
 package controllers;
 
+import fuschia.tagger.Bootstrap;
 import play.mvc.*;
 
 public class Survey extends Controller {
 
     public static void index() {
-        render();
+    	String participantId;
+    	do {
+    		participantId = Bootstrap.DefaultDocumentRepository.getParticipantMap().getRandomS1Key();
+    	} while (participantId.endsWith("B"));
+    	
+    	String strSurveyContent = Bootstrap.DefaultDocumentRepository.getDocumentById(participantId+"-Q1").getText();
+        render(participantId, strSurveyContent);
     }
 
 }
